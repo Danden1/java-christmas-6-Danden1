34988db80fdff97f2b28c4da7a1b26ba9c9fd009
권용하 <yongha.kwon12@gmail.com>
2023-11-15 22:07:36 +0900

    feat: main 함수 추가

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,45 @@
 package christmas;
 
+import christmas.application.OrderService;
+import christmas.domain.discount.*;
+import christmas.domain.menu.utils.MenuFinder;
+import christmas.domain.order.Order;
+import christmas.domain.order.OrderImpl;
+import christmas.presentation.OrderController;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Order order = new OrderImpl(discounters());
+        OrderService orderService = new OrderService(order, new MenuFinder());
+        OrderController orderController = new OrderController(orderService);
+        orderController.gameStart();
+    }
+
+    private static List<Discounter> discounters() {
+        List<Discounter> discounters = new ArrayList<>();
+        discounters.add(new ChristmasDiscounter());
+        discounters.add(new WeekDiscounter());
+        discounters.add(new SpecialDiscounter(specialDates()));
+        discounters.add(new FreeBieDiscounter());
+
+        return discounters;
+    }
+
+    private static Set<LocalDate> specialDates() {
+        Set<LocalDate> specialDates = new HashSet<>();
+        specialDates.add(LocalDate.of(2023,12,3));
+        specialDates.add(LocalDate.of(2023,12,10));
+        specialDates.add(LocalDate.of(2023,12,17));
+        specialDates.add(LocalDate.of(2023,12,24));
+        specialDates.add(LocalDate.of(2023,12,25));
+        specialDates.add(LocalDate.of(2023,12,31));
+
+        return specialDates;
     }
 }
