cc5d68be545f3f5d65045305e2b925b0928896e3
권용하 <yongha.kwon12@gmail.com>
2023-11-15 22:06:37 +0900

    refactor: 이름 오타 수정

--- src/main/java/christmas/domain/order/OrderErrorMessage.java
+++ src/main/java/christmas/domain/order/OrderErrorMessage.java
@@ -1,7 +1,7 @@
 package christmas.domain.order;
 
 public enum OrderErrorMessage {
-    INVALID_ORDER("[ERROR] 유효하지 않은 주문입니다. 다시 입력해주세요.");
+    INVALID_ORDER("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
 
     private final String message;
 
